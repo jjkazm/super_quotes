5f63e6247e6081f44019369438529c016fd1e464
jjkazm <jjkazm@gmail.com>
2019-01-13 19:59:29 +0100

    Add Quotes controller and index, new, create methods

---
+++ app/controllers/quotes_controller.rb
@@ -0,0 +1,24 @@
+class QuotesController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @quote = Quote.new
+  end
+
+  def create
+    @quote = Quote.new(qoute_params)
+    if @quote.save
+      flash[:success] = "Quote has been added"
+      redirect_to quotes_path
+    else
+      render 'new'
+  end
+
+  private
+    def quote_params
+      params.require(:quote).permit(:body, :source)
+    end
+
+end
