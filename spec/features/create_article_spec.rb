0427c8ad38e4b1f28a4e61635153a0e15eb8c2e0
jjkazm <jjkazm@gmail.com>
2019-01-14 11:24:30 +0100

    Add show quote view in order to prepare it for comments adding feature

--- spec/features/create_article_spec.rb
+++ spec/features/create_article_spec.rb
@@ -9,10 +9,14 @@ RSpec.feature "Creating quote" do
 
     fill_in "Body", with: "Just another quote"
     fill_in "Source", with: "Somebody said that"
+    fill_in "Author", with: "the book"
 
     click_button "Create Quote"
 
     expect(page).to have_content("Quote has been added")
+    expect(page).to have_content("Just another quote")
+    expect(page).to have_content("Somebody said that")
+    expect(page).to have_content("the book")
     expect(page.current_path).to eq(quotes_path)
   end
 end
