a40247bc731a421937c739d2e781ffeb47b4d61a
jjkazm <jjkazm@gmail.com>
2019-01-14 01:48:54 +0100

    Add author for quote model

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,15 @@
 class Quote < ApplicationRecord
+
+
+  def introduction
+    # if self.author.empty? && self.source.empty?
+    #   "Anonymous"
+    # elsif self.author.empty?
+    #   "Someone in #{self.source}"
+    # elsif self.source.empty?
+    #   self.author
+    # else
+    #   "#{self.author} in #{self.source}"
+    # end      
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_13_182600) do
+ActiveRecord::Schema.define(version: 2019_01_14_003938) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_01_13_182600) do
     t.string "source"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "author"
   end
 
 end
