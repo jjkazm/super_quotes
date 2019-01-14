d6037d4d2deccf34a0dc3bdc544d9b48af2b3abf
jjkazm <jjkazm@gmail.com>
2019-01-14 14:28:37 +0100

    Seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+6.times do |n|
+  Quote.create(body: Faker::BackToTheFuture.quote, source: "Back to the future", author: Faker::BackToTheFuture.character)
+  Quote.create(body: Faker::DrWho.quote, source: "Dr Who", author: Faker::DrWho.character)
+  Quote.create(body: Faker::DumbAndDumber.quote, source: "Dumb and dumber", author: Faker::DumbAndDumber.character)
+  Quote.create(body: Faker::FamilyGuy.quote, source: "Back to the future", author: Faker::FamilyGuy.character)
+end
+
+Quote.all.each do |quote|
+  rand(20).times do |x|
+    quote.comments.create(body: Faker::Lorem.sentence)
+  end
+end
